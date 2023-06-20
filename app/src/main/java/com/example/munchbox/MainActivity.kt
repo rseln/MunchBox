49260e62cc85293233c726407f41cc05b2e0efb4
Eric Bilaver <ebilaver@uwaterloo.ca>
2023-06-20 14:46:35 -0400

    added basic framework

--- app/src/main/java/com/example/munchbox/MainActivity.kt
+++ app/src/main/java/com/example/munchbox/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.munchbox.ui.components.MealCardContainer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.munchbox.ui.NumberOfMealsScreen
+import com.example.munchbox.ui.theme.GreetingCardTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MealCardContainer()
+//            MealCardContainer()
+//            NumberOfMealsScreen()
         }
     }
 }
