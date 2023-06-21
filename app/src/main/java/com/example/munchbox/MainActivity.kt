12b46f913e3b23b81a4fd88cb9c87221f522c7de
Karim Al-Atrash <karimalatrash@gmail.com>
2023-06-21 13:30:15 -0400

    added meal card

--- app/src/main/java/com/example/munchbox/MainActivity.kt
+++ app/src/main/java/com/example/munchbox/MainActivity.kt
@@ -3,19 +3,14 @@ package com.example.munchbox
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.ui.Modifier
-import com.example.munchbox.ui.components.RestaurantCard
-import com.example.munchbox.ui.theme.GreetingCardTheme
+import com.example.munchbox.ui.components.MealCardContainer
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RestaurantCard(name = "Lazeez")
+            MealCardContainer()
         }
     }
 }
--- app/src/main/java/com/example/munchbox/controller/Restaurant.kt
+++ app/src/main/java/com/example/munchbox/controller/Restaurant.kt
@@ -1,25 +1,8 @@
 package com.example.munchbox.controller
 
-enum class DietaryOption(val id: Int, val str : String) {
-    HALAL(0, "Halal"),
-    VEGE(1, "Vegetarian"),
-    KOSHER(2, "Kosher"),
-    VEGAN(3, "Vegan"),
-    GF(4, "Gluten Free"),
-    PEANUT_FREE(5, "Peanut Free"),
-    MEAT(6, "Meat")
-}
-
-class Meal(options : Array<DietaryOption>) {
-    fun optionToStr(option : DietaryOption): String {
-        return option.str
-    }
-}
-
-class Restaurant(name : String) {
+class Restaurant(val name : String) {
     val meals : MutableList<Meal> = mutableListOf()
     fun addMeal(meal : Meal) {
         meals.add(meal)
     }
-
 }
