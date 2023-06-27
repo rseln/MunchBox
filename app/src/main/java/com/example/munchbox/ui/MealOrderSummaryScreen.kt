9bea9c7ec8f911c3400c6baad3b66c90367a200f
Weixue Chen <sean.chen0818@gmail.com>
2023-06-27 03:35:06 -0400

    cleaned up UI

--- app/src/main/java/com/example/munchbox/ui/MealOrderSummaryScreen.kt
+++ app/src/main/java/com/example/munchbox/ui/MealOrderSummaryScreen.kt
@@ -1,6 +1,8 @@
 package com.example.munchbox.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,18 +12,24 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.munchbox.R
 import com.example.munchbox.controller.DayOfWeek
 import com.example.munchbox.controller.Meal
 import com.example.munchbox.data.DataSource.allMeals
@@ -39,7 +47,7 @@ fun MealOrderSummaryScreen(
 ) {
     val scrollState = rememberScrollState()
     Column(
-        modifier = modifier
+        modifier = Modifier
         .verticalScroll(scrollState),
     ){
         Spacer(modifier = Modifier.height(32.dp))
@@ -51,24 +59,28 @@ fun MealOrderSummaryScreen(
             }
         }
         Spacer(modifier = Modifier.height(32.dp))
-        OrdersAvailable(orderUiState)
-        OrderSummaries(orderUiState)
+        OrdersAvailable(orderUiState, modifier)
+        OrderSummaries(orderUiState, modifier)
     }
 }
 
 @Composable
-fun OrdersAvailable(order: OrderUiState){
-    Row(
-        horizontalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxWidth(),
-    ) {
-        Text(
-            text = "Orders Available for Pickup",
-            fontWeight = FontWeight.Bold,
-            fontSize = 21.sp,
-            style = Typography.headlineSmall,
-            textAlign = TextAlign.Center,
-        )
+fun OrdersAvailable(order: OrderUiState, modifier: Modifier = Modifier){
+    Row{
+        Box(
+            modifier = Modifier
+                .background(color = Color(118, 97, 173))
+                .padding(16.dp)
+                .fillMaxWidth()
+        ){
+            Text(
+                text = "Orders Available for Pickup",
+                fontWeight = FontWeight.Bold,
+                fontSize = 21.sp,
+                color = Color.White,
+                style = Typography.headlineSmall,
+            )
+        }
     }
 
     if (order.pickupOptions.isEmpty()){
@@ -76,30 +88,34 @@ fun OrdersAvailable(order: OrderUiState){
     }
     for(pickupDate in order.pickupOptions){
         if (currentDay == pickupDate){
-            for(meal in order.meals){
-                OrderSummaryCard(meal = meal)
+            Column(modifier = modifier) {
+                for(meal in order.meals){
+                    OrderSummaryCard(meal = meal)
+                }
             }
         }
     }
-
-
 }
 
 @Composable
-fun OrderSummaries(order: OrderUiState){
-    Row(
-        horizontalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxWidth(),
-    ) {
-        Text(
-            text = "Order Summaries",
-            fontWeight = FontWeight.Bold,
-            fontSize = 21.sp,
-            style = Typography.headlineSmall,
-            textAlign = TextAlign.Center,
-        )
+fun OrderSummaries(order: OrderUiState, modifier: Modifier = Modifier){
+    Row{
+        Box(
+            modifier = Modifier
+                .background(color = Color(118, 97, 173) )
+                .padding(16.dp)
+                .fillMaxWidth(),
+        ) {
+            Text(
+                text = "Order Summaries",
+                fontWeight = FontWeight.Bold,
+                fontSize = 21.sp,
+                style = Typography.headlineSmall,
+                textAlign = TextAlign.Center,
+                color = Color.White,
+            )
+        }
     }
-
     /** Map of days in the week to a set of meals **/
     /** {MONDAY: {MEAL1, MEAL2}}**/
     val dayFilterMap = mutableMapOf<DayOfWeek,MutableSet<Meal>>()
@@ -114,16 +130,17 @@ fun OrderSummaries(order: OrderUiState){
         dayFilterMap.contains(it)
     }
     for (day in daysWithOrders) {
-        Text(
-            text = day.str,
-            fontWeight = FontWeight.Bold,
-            fontSize = 18.sp,
-            modifier = Modifier.padding(start=16.dp),
-        )
-        for(meal in dayFilterMap[day]!!){
-            OrderSummaryCard(meal = meal)
+        Column(modifier = modifier) {
+            Text(
+                text = day.str,
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                modifier = Modifier.padding(start=16.dp),
+            )
+            for(meal in dayFilterMap[day]!!){
+                OrderSummaryCard(meal = meal)
+            }
         }
-
     }
 }
 
