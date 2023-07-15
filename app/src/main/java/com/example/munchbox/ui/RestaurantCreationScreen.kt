9ceca94d6418ae325302889768b4dfed2e9da467
Eric Bilaver <ebilaver@uwaterloo.ca>
2023-07-15 18:31:49 -0400

    added basic chip handling for dietaryOptions

--- app/src/main/java/com/example/munchbox/ui/RestaurantCreationScreen.kt
+++ app/src/main/java/com/example/munchbox/ui/RestaurantCreationScreen.kt
@@ -5,20 +5,31 @@ package com.example.munchbox.ui
 import android.app.TimePickerDialog
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,9 +39,22 @@ import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.munchbox.controller.DietaryOption
 import java.util.Calendar
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
-@OptIn(ExperimentalMaterial3Api::class)
+val dietaryOptions = setOf(
+    Pair(0, "Halal"),
+    Pair(1, "Vegetarian"),
+    Pair(2, "Kosher"),
+    Pair(3, "Vegan"),
+    Pair(4, "Gluten Free"),
+    Pair(5, "Peanut Free"),
+    Pair(6, "Meat")
+)
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 fun RestaurantCreationScreen() {
     val mContext = LocalContext.current
@@ -99,10 +123,43 @@ fun RestaurantCreationScreen() {
         // CUISINE
         Text(text = "Cuisine")
 
+
+
         Spacer(modifier = Modifier.padding(20.dp))
 
         // DIETARY OPTIONS
         Text(text = "Dietary Offerings")
+
+        var selectedOptions by remember { mutableStateOf(setOf<Pair<Int, String>>()) }
+
+        FlowRow(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            for (option in dietaryOptions) {
+                FilterChip(
+                    selected = selectedOptions.contains(option),
+                    label = {Text(option.second)},
+                    onClick = {
+                        if (selectedOptions.contains(option)) {
+                            selectedOptions = selectedOptions.minus(option)
+                        }
+                        else {
+                            selectedOptions = selectedOptions.plus(option)
+                        }
+                    },
+                    leadingIcon = {
+                        if (selectedOptions.contains(option)) {
+                            Icon(
+                                imageVector = Icons.Filled.Done,
+                                contentDescription = "Localized Description",
+                                modifier = Modifier.size(FilterChipDefaults.IconSize)
+                            )
+                        }
+                    }
+                )
+                Spacer(modifier = Modifier.size(4.dp, 4.dp))
+            }
+        }
 
         Spacer(modifier = Modifier.padding(20.dp))
 
@@ -115,14 +172,9 @@ fun RestaurantCreationScreen() {
         )
         Spacer(modifier = Modifier.padding(20.dp))
 
-        // EMAIL
+        // EMAIL - done
         Text(text = "Business Email")
         TextField(value = restaurantEmail.value, onValueChange = {restaurantEmail.value = it}, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email))
-        Spacer(modifier = Modifier.padding(20.dp))
-
-        // PAYMENT
-        Text(text = "Link Payment Account ")
-        // TODO
         Spacer(modifier = Modifier.padding(20.dp))
 
         // FINISH
