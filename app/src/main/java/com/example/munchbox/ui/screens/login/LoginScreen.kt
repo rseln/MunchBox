1434be5ac129dfad1c7216da2394605cb8d45a48
Seanlebon <sean.chen0818@gmail.com>
2023-07-15 23:33:40 -0400

    removed muchbox viewmodel. We might not need to change repo file structure unless it gets really wonky

--- app/src/main/java/com/example/munchbox/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/munchbox/ui/screens/login/LoginScreen.kt
@@ -16,7 +16,6 @@ package com.example.munchbox.ui.screens.login
  * limitations under the License.
  */
 import android.content.Context
-import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -37,12 +36,9 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -60,12 +56,7 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.munchbox.R
-import com.example.munchbox.controller.Restaurant
-import com.example.munchbox.data.RestaurantStorageService
-import com.example.munchbox.ui.screens.MunchBoxViewModel
-import com.google.firebase.firestore.FirebaseFirestore
 
 /**
  * Composable for the login page.
