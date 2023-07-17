0327d048a93b92a740c81abb814680063897a047
Anees Aissaoui <aissaoui.anees@gmail.com>
2023-07-17 14:11:03 -0400

    removed some dead imports

--- app/src/main/java/com/example/munchbox/data/StorageServices.kt
+++ app/src/main/java/com/example/munchbox/data/StorageServices.kt
@@ -1,19 +1,6 @@
 package com.example.munchbox.data
 
-import android.util.Log
-import androidx.compose.ui.text.toLowerCase
-import androidx.lifecycle.ViewModel
-import com.example.munchbox.controller.Meal
-import com.example.munchbox.controller.Restaurant
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreException
-import com.google.firebase.firestore.SetOptions
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.tasks.await
-import kotlinx.coroutines.withContext
-import java.util.UUID
 import javax.inject.Inject
 
 // TODO: replace all List<Meal> with List<String> once meal IDs are implemented
