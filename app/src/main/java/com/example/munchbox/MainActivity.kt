df98ef51debe3bd48248f4b573a4e5ed08310451
Anushka Garg <anushka.7901@gmail.com>
2023-07-20 15:44:01 -0400

    google sign in and email/password auth working

--- app/src/main/java/com/example/munchbox/MainActivity.kt
+++ app/src/main/java/com/example/munchbox/MainActivity.kt
@@ -5,7 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.munchbox.ui.theme.MunchBoxTheme
 import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/munchbox/data/ClientID.kt
@@ -0,0 +1,6 @@
+package com.example.munchbox.data
+
+object ClientID {
+
+    const val ServerClient = "126924448294-cufvev89c5d97nojhuqu18435elf8310.apps.googleusercontent.com"
+}
