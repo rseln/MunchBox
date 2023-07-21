d403549ac2e6c95f613550ad21e8594344b5bc13
Anushka Garg <anushka.7901@gmail.com>
2023-07-21 18:52:15 -0400

    user API and collection done, need to figure out restaurant creation flow

--- app/src/main/java/com/example/munchbox/data/UserStorageService.kt
+++ app/src/main/java/com/example/munchbox/data/UserStorageService.kt
@@ -1,28 +1,32 @@
+
 package com.example.munchbox.data
 
 import android.util.Log
 import com.example.munchbox.controller.User
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
+import java.util.UUID
 import javax.inject.Inject
 
 class UserStorageService
-//@Inject
 
-/*constructor(private val firestore: FirebaseFirestore) {
+@Inject
 
+constructor(private val firestore: FirebaseFirestore) {
     suspend fun createDBUser (
         userID:String,
         email:String,
         type:String,
-        restaurantID: String
+        restaurantID: String? = null
     ): User? = withContext(Dispatchers.IO) {
 
-        val userID:String = Firebase.auth.currentUser.uid.String()
+        val userID:String = Firebase.auth.currentUser?.uid ?: ""
 
         val data = hashMapOf(
             "userID" to userID,
@@ -34,12 +38,46 @@ class UserStorageService
         try {
             firestore.collection("Users").document(userID).set(data)
             return@withContext User(userID, email, type, restaurantID)
-        }catch(e: FirebaseFirestoreException) {
+        } catch(e: FirebaseFirestoreException) {
             Log.e("FIRESTORE ERROR", e.message.toString())
         }
+        return@withContext null
 
     }
-    return@withContext null
 
-}*/
+    suspend fun getUserByUserID(userID:String): User? = withContext(Dispatchers.IO) {
+        try {
+            val userSnapshot = firestore.collection("Users").document(userID).get().await()
+            if(userSnapshot.exists()) {
+                val data = userSnapshot.data
+                val id = data?.get("userID") as? String ?: ""
+                val email = data?.get("email") as? String ?: ""
+                val type = data?.get("type") as? String ?: ""
+                val restaurantId = data?.get("restaurantID") as? String ?: ""
+
+                return@withContext User(id, email, type, restaurantId)
+            }
+        } catch(e: FirebaseFirestoreException) {
+            Log.e("FIRESTORE ERROR", e.message.toString())
+        }
+        return@withContext null
+    }
+
+    suspend fun getTypeByUserID(userID:String): String? = withContext(Dispatchers.IO) {
+        try {
+            val userSnapshot = firestore.collection("Users").document(userID).get().await()
+            if(userSnapshot.exists()) {
+                val data = userSnapshot.data
+                val type = data?.get("type") as? String ?: ""
+
+                return@withContext type
+            }
+        } catch(e: FirebaseFirestoreException) {
+            Log.e("FIRESTORE ERROR", e.message.toString())
+        }
+        return@withContext null
+    }
+
+}
+
 
